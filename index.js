3ab2d3b1f27e48828b7a01e9ba551fbde0fc5b37
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2025-09-18 23:44:55 +0600

    feat: show specific plants details

--- index.js
+++ index.js
@@ -128,6 +128,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+
+      const result = await plantsCollection.findOne(filter)
+      res.send(result)
+    })
+
 
 
 
